6a773b2f9e4ee290a907be0e2f893300b84e10f7
jainish2222 <jainishkoladiya.it21@scet.ac.in>
2024-02-19 20:05:30 +0530

    Merge unrelated histories

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 // src/App.js
-import React from 'react';
-import SignUpForm from './components/SignUpForm';
+import React, { useState } from 'react';
+import NotesList from './components/NotesList';
+import AddNoteForm from './components/AddNoteForm';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  const handleAddNote = (newNote) => {
+    setNotes([...notes, newNote]);
+  };
+
+  const handleDeleteNote = (id) => {
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+  };
+
   return (
-    <div className="App">
-      <h1 className='pp1'>Sign-Up Form</h1>
-      <SignUpForm />
+    <div className="app">
+      <h1>Notes App</h1>
+      <AddNoteForm onAdd={handleAddNote} />
+      <NotesList notes={notes} onDelete={handleDeleteNote} />
     </div>
   );
-}
+};
 
 export default App;
